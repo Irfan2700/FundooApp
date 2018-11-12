df457e597f409c0099f0e2188789d985a9ab10fe
Irfan Rizvi <sirizvi2700@gmail.com>
2018-11-12 19:52:42 +0530

    12/11/18

--- src/app/components/create-remainder/create-remainder.component.ts
+++ src/app/components/create-remainder/create-remainder.component.ts
@@ -1,6 +1,8 @@
+import { EventEmitter, Output } from '@angular/core';
+// import { LoggerService } from 'src/app/core/services/logger.service';
 import { LoggerService } from './../../core/services/logger.service';
 import { NoteServicesService } from './../../core/services/note-services.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -12,6 +14,9 @@ export class CreateRemainderComponent implements OnInit {
 
   constructor(private noteService: NoteServicesService) { }
 
+  @Input() note;
+  @Output() update = new EventEmitter();
+
   settingDate;
   flag = false;
   date = new FormControl(new Date());
@@ -19,17 +24,48 @@ export class CreateRemainderComponent implements OnInit {
   
   setDate;
 
-  requestBody = {
-    "remainder": this.settingDate,
-    "noteIdList": []
-  }
+   requestBody; 
+   //= {
+  //   "remainder": ,
+  //   "noteIdList": []
+  // }
 
   dat = new Date();
+
+  quickRemainder(time){
+    if(time === "8:00 PM"){
+      this.settingDate = new Date(this.dat.getFullYear(), this.dat.getMonth(), this.dat.getDate() + 0, 20, 0, 0);
+    }else if(time === "8:00 AM"){
+      this.settingDate = new Date(this.dat.getFullYear(), this.dat.getMonth(), this.dat.getDate() + 1, 8, 0, 0);
+    }else if(time === "MON, 8:00 AM"){
+      this.settingDate = new Date(this.dat.getFullYear(), this.dat.getMonth(), this.dat.getDate() + 7, 8, 0, 0);
+    }
+
+    this.requestBody = {
+      "reminder": this.settingDate,
+      "noteIdList": [this.note.id]
+    }
+
+    this.noteService.addRemainder(this.requestBody).subscribe(
+      response => {
+        LoggerService.logObj("the remainder is added successfully", response)
+        this.update.emit({});
+      },
+      error => {
+        LoggerService.log("Error Occured!!");
+      }
+    )
+  }
   
 
   pickSetTime(weekday){
+    
+    // this.setDate.
+
     if(weekday === "Morning"){
       this.pickTime = "8:00 AM";
+
+      this.settingDate = new Date()
     }else if(weekday === "Afternoon"){
       this.pickTime = "1:00 PM";
     }else if(weekday === "Evening"){
@@ -45,22 +81,26 @@ export class CreateRemainderComponent implements OnInit {
   }
 
   today(){
-    this.settingDate = new Date(this.dat.getFullYear(), this.dat.getMonth(), this.dat.getDate() + 0, 20, 0, 0);
+    // this.settingDate = new Date(this.dat.getFullYear(), this.dat.getMonth(), this.dat.getDate() + 0, 20, 0, 0);
 
     // this.remainder.remainderDate = this.settingDate;
     // this.remainder.remainderTime = ;
 
-    this.noteService.addRemainder(this.settingDate).subscribe(
-      response => {
-        LoggerService.logObj("remainder is successfully added to the Note", response)
-      }
-    )
+    // this.noteService.addRemainder(this.settingDate).subscribe(
+    //   response => {
+    //     LoggerService.logObj("remainder is successfully added to the Note", response)
+    //   }
+    // )
   }
 
   ngOnInit() {
-    // console.log(this.date)
+    // console.log(this.date.value.toISOString())
+    let d = new Date(2017,11,3,20,23,0,0);
+    console.log(d.toISOString())
+    console.log(d)
     
-    this.setDate = this.date.value;
+    this.setDate = this.date;
+
     // LoggerService.logObj("testing processing", this.setDate)
   }
 
